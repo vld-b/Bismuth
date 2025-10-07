86669f6d51d12f7bfcc6503881f9da3f1e11b472
Vlad <vladbodea2007@gmail.com>
2025-10-07 13:47:45 +0200

    feat(note): Begin implementation of notes deletion

--- WID/NotebookPage.xaml.cs
+++ WID/NotebookPage.xaml.cs
@@ -32,14 +32,18 @@ namespace WID
         public InkPresenterRuler ruler { get; private set; }
         public InkPresenterProtractor protractor { get; private set; }
 
-        public NotebookPage(int id)
+        public NotebookPage()
         {
             this.InitializeComponent();
-            this.id = id;
             canvas = inkCanvas;
             inkPres = inkCanvas.InkPresenter;
             ruler = new InkPresenterRuler(inkPres);
             protractor = new InkPresenterProtractor(inkPres);
+        }
+
+        public NotebookPage(int id) : this()
+        {
+            this.id = id;
         }
 
         public NotebookPage(int id, double width, double height) : this(id)
