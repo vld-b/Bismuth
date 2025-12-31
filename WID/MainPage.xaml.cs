e2b8c8648623d315616b4bd3ddd5b572f1f99a11
Vlad <vladbodea2007@gmail.com>
2025-12-31 16:06:32 +0100

    fix(main): Reverse branchless page switch

--- WID/MainPage.xaml.cs
+++ WID/MainPage.xaml.cs
@@ -31,22 +31,10 @@ namespace WID
     public sealed partial class MainPage : Page
     {
         public StorageFolder notes = ApplicationData.Current.LocalFolder;
-
-        private readonly Dictionary<string, Type> pages = new Dictionary<string, Type>
-        {
-            ["notebooksPage"] = typeof(NotebookList),
-            ["Settings"] = typeof(SettingsPage),
-        };
-
-        private readonly Dictionary<string, object?> pageParams = new Dictionary<string, object?>
-        {
-            ["Settings"] = null,
-        };
         public MainPage()
         {
             InitializeComponent();
             SetTitlebar();
-            pageParams.Add("notebooksPage", new FolderNavigationData(null, Frame));
 
             nvMainNavigation.SelectedItem = nvMainNavigation.MenuItems[0];
         }
@@ -71,29 +59,23 @@ namespace WID
         {
             if (args.SelectedItem is Microsoft.UI.Xaml.Controls.NavigationViewItem item)
             {
-                if (pages.TryGetValue(item.Tag.ToString()!, out Type? page))
-                    frMainMenu.Navigate(
-                        page,
-                        pageParams[item.Tag.ToString()!],
-                        new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight }
-                        );
-                //switch (item.Tag)
-                //{
-                //    case "notebooksPage":
-                //        frMainMenu.Navigate(
-                //            typeof(NotebookList),
-                //            new FolderNavigationData(null, Frame),
-                //            new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight }
-                //            );
-                //        break;
-                //    case "Settings":
-                //        frMainMenu.Navigate(
-                //            typeof(SettingsPage),
-                //            null,
-                //            new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight }
-                //            );
-                //        break;
-                //}
+                switch (item.Tag)
+                {
+                    case "notebooksPage":
+                        frMainMenu.Navigate(
+                            typeof(NotebookList),
+                            new FolderNavigationData(null, Frame),
+                            new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight }
+                            );
+                        break;
+                    case "Settings":
+                        frMainMenu.Navigate(
+                            typeof(SettingsPage),
+                            null,
+                            new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight }
+                            );
+                        break;
+                }
             }
         }
     }
