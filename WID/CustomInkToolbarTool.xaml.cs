d9a6166bd044786da52ffa2dc1b15d1ff1f50df8
vld-b <vladbodea2007@gmail.com>
2026-05-01 03:29:04 +0200

    feat(notes): Add sublte gradient on selected ink tool

--- WID/CustomInkToolbarTool.xaml.cs
+++ WID/CustomInkToolbarTool.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -49,7 +51,7 @@ namespace WID
                 if (_isSelected)
                 {
                     gdContent.Translation = new System.Numerics.Vector3(0f, -4f, 0f);
-                    Background = (SolidColorBrush)Application.Current.Resources["SystemControlHighlightAccentBrush"];
+                    Background = bg;
                 } else
                 {
                     gdContent.Translation = new System.Numerics.Vector3(0f, 0f, 0f);
@@ -58,11 +60,30 @@ namespace WID
             }
         }
 
+        LinearGradientBrush bg;
+
         public CustomInkToolbarTool()
         {
             this.InitializeComponent();
 
             Children = new ObservableCollection<UIElement>();
+
+            bg = new LinearGradientBrush();
+            bg.StartPoint = new Point(.3f, 0);
+            bg.EndPoint = new Point(.7f, 1);
+            bg.ColorInterpolationMode = ColorInterpolationMode.ScRgbLinearInterpolation;
+
+            Color accentColor = (Color)Application.Current.Resources["SystemAccentColor"];
+            Color startColor = CreateClampedColor(255, accentColor.R + 30, accentColor.G + 30, accentColor.B + 30);
+            Color endColor = CreateClampedColor(255, accentColor.R - 10, accentColor.G - 10, accentColor.B - 10);
+
+            bg.GradientStops.Add(new GradientStop { Color = startColor, Offset = 0.0d });
+            bg.GradientStops.Add(new GradientStop { Color = endColor, Offset = 1.0d });
+        }
+
+        private Color CreateClampedColor(int a, int r, int g, int b)
+        {
+            return Color.FromArgb((byte)Math.Clamp(a, 0, 255), (byte)Math.Clamp(r, 0, 255), (byte)Math.Clamp(g, 0, 255), (byte)Math.Clamp(b, 0, 255));
         }
     }
 }
