b3df4df89126d77b785b451cd51ba41e6027fc37
vld-b <vladbodea2007@gmail.com>
2026-09-22 20:24:46 +0200

    fix(compilation): Try fixing compilation of the shaders again

--- Graphics/BuildShaders.bat
+++ Graphics/BuildShaders.bat
@@ -1,14 +1,14 @@
 :: 1. Check if VSINSTALLDIR or VSAPPIDDIR is already set by MSBuild/VS
 if defined VSINSTALLDIR (
-    call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
+    call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat" -arch=%~1
 ) else if defined VSAPPIDDIR (
-    call "%VSAPPIDDIR%..\..\Common7\Tools\VsDevCmd.bat"
+    call "%VSAPPIDDIR%..\..\Common7\Tools\VsDevCmd.bat" -arch=%~1
 ) else (
     :: 2. Fallback: Use vswhere.exe (pre-installed on VS and GitHub Runners) to dynamically locate VS!
     set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
     if exist "!VSWHERE!" (
         for /f "usebackq tokens=*" %%i in (`"!VSWHERE!" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-            call "%%i\Common7\Tools\VsDevCmd.bat"
+            call "%%i\Common7\Tools\VsDevCmd.bat" -arch=%~1
         )
     )
 )
@@ -21,5 +21,5 @@ set FXC_PATH="%WindowsSdkDir%\bin\%WindowsSDKVersion%\%~1\fxc.exe"
 
 echo "%WindowsSdkDir%\bin\%WindowsSDKVersion%\%~1\fxc.exe"
 
-%FXC_PATH% SearchNotesBackground.hlsl /nologo /T lib_4_0 /D D2D_FUNCTION /D D2D_ENTRY=main /Fl SearchNotesBackground.fxlib /I %INCLUDEPATH%
-%FXC_PATH% SearchNotesBackground.hlsl /nologo /T ps_4_0 /D D2D_FULL_SHADER /D D2D_ENTRY=main /E main /setprivate SearchNotesBackground.fxlib /Fo:SearchNotesBackground.bin /I %INCLUDEPATH%
+fxc SearchNotesBackground.hlsl /nologo /T lib_4_0 /D D2D_FUNCTION /D D2D_ENTRY=main /Fl SearchNotesBackground.fxlib /I %INCLUDEPATH%
+fxc SearchNotesBackground.hlsl /nologo /T ps_4_0 /D D2D_FULL_SHADER /D D2D_ENTRY=main /E main /setprivate SearchNotesBackground.fxlib /Fo:SearchNotesBackground.bin /I %INCLUDEPATH%
